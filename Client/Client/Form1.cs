345b91cdc8dd113255b5d37343516b74a1344368
damato.20110 <damato.20110@LABI1-13PC03>
2024-10-30 09:11:03 +0100

    Aggiungere i file di progetto.

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public static string data = "";
+        public static IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+        public static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+        public static Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        public static byte[] bytes = new byte[1024];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartClient();
+        }
+        public static void StartClient()
+        {
+            try
+            {
+                try
+                {
+                    sender.Connect(remoteEP);
+                    //sender.Shutdown(SocketShutdown.Both);
+                    //sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        public static string data = null;
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+
+        public static void StartListening()
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Timeout : {0}", listener.ReceiveTimeout);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket handler = listener.Accept();
+
+                    ClientManager clientThread = new ClientManager(handler);
+                    Thread t = new Thread(new ThreadStart(clientThread.doClient));
+                    t.Start();
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+    }
+    public class ClientManager
+    {
+
+        Socket clientSocket;
+        byte[] bytes = new Byte[1024];
+        String data = "";
+
+        public ClientManager(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+        }
+
+        public void doClient()
+        {
+
+            while (data != "Quit$")
+            {
+                Console.WriteLine("informazioni client : " + clientSocket.RemoteEndPoint);
+                data = "";
+                while (data.IndexOf("$") == -1)
+                {
+                    int bytesRec = clientSocket.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                }
+
+                Console.WriteLine("Messaggio ricevuto : {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                clientSocket.Send(msg);
+            }
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            data = "";
+        }
+    }
+}
